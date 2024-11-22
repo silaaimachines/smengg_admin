2aeab7fd0afe0363431109c72a18a330b9a37ba6
Marshal Tudu <iammarshal22@gmail.com>
2024-11-22 12:07:15 +0530

    xxx

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: "Silaaimachines Strapi",
+      script: "node",
+      args: '"C:\\Program Files\\nodejs\\node_modules\\npm\\bin\\npm-cli.js" run develop',
+    },
+  ],
+};
